fa2d3a975290545acc8321dbae18bdf2a18285c1
UIF <UIF@uif.org>
2024-02-07 06:12:05 +0000

    auto update

---
+++ src/uif/template/speedtest.js
@@ -0,0 +1,36 @@
+import {
+  DeepCopy
+} from '@/utils'
+
+import {
+  Outbound
+} from '@/store/uif/uif2singbox.js'
+
+export var template = {
+  "experimental": {
+    "clash_api": {
+      "external_controller": '127.0.0.1:pportportportportportort'
+    }
+  },
+  "dns": {
+    "servers": [{
+      "tag": "dns_direct",
+      "address": "udp://114.114.114.114",
+      "detour": "freedom"
+    }],
+    "independent_cache": true
+  },
+  "outbounds": [{
+    'tag': 'freedom',
+    'type': 'direct'
+  }]
+}
+
+export function BuildTestNodeTemplate(uifStyleNodeConfig) {
+  var res = DeepCopy(template)
+  var rawOutboudList = []
+  for (var item in uifStyleNodeConfig) {
+    res['outbounds'].push(Outbound(uifStyleNodeConfig[item]))
+  }
+  return res
+}
